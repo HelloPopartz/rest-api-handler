4f4c6effefc895bd140859a3ecc4f2a76e2dfc4c
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2019-06-28 13:20:50 +0200

    Add default export for redux package

--- packages/rest-api-handler-redux/src/index.ts
+++ packages/rest-api-handler-redux/src/index.ts
@@ -1,2 +1,6 @@
-export { createConnectedResource } from './connectedRestResource'
+import { createConnectedResource } from './connectedRestResource'
+
 export { connectToRestResources } from './restStoreEnhancer'
+
+export default createConnectedResource
+export { createConnectedResource }
